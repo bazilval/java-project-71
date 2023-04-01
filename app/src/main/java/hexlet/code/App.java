f864c9f3339b135559be70a2be02f488c3ba4b82
Valentin Bazilevich <bazilvalentin@gmail.com>
2023-04-01 22:29:44 +0300

    Add picocl and fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,12 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendff 1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
