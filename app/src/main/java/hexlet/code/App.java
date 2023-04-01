478e3e52271bb8351d835fb4b0679ece2220ce48
Valentin Bazilevich <bazilvalentin@gmail.com>
2023-04-01 22:49:07 +0300

    Add console parameters

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,19 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendff 1.0",
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    String format = "stylish";
+    @Parameters(index = "0", description = "path to first file")
+    String filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    String filepath2;
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
